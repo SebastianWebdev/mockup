e6b3bc13ddeda69fefc80a2849ee08194d89f612
Sebastian <sebastian.golebiowski@tensquaregames.com>
2026-02-10 15:23:02 +0100

    das

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? "/prosecco-mockup/" : "/",
+  base: process.env.GITHUB_ACTIONS ? "/mockup/" : "/",
   plugins: [react()],
   server: {
     port: 3000,
